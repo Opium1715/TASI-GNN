c0e559f52d6c2c87677b6e82eea010ba2f6215f8
Opium1715 <284067302@qq.com>
2024-01-06 19:38:55 +0800

    编写中

--- model/model.py
+++ model/model.py
@@ -1,6 +1,9 @@
+import math
+
 import torch
 from torch import nn
 import torch.nn.functional as F
+from entmax import entmax_bisect
 
 
 class GGNN(nn.Module):
@@ -24,6 +27,51 @@ class GGNN(nn.Module):
         inputs = torch.concat([adj_in, adj_out], -1)
         return self.gnn_cell(inputs)
 
+
+class S_ATT(nn.Module):
+    def __init__(self, emb_size, drop_out, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.emb_size = emb_size
+        self.linear_q = nn.Linear(2 * self.emb_size, 2 * self.emb_size)
+        self.drop_out = drop_out
+        self.scale = math.sqrt(self.emb_size * 2)
+        self.linear_alpha = nn.Linear(self.emb_size, 1)
+        self.ffn = nn.Sequential(nn.Linear(2 * self.emb_size, 2 * self.emb_size),
+                                 nn.ReLU(),
+                                 nn.Linear(2 * self.emb_size, 2 * self.emb_size),
+                                 nn.Dropout(self.drop_out))
+        self.layer_norm = nn.LayerNorm(2 * self.emb_size, eps=1e-12)
+
+    def forward(self, X):
+        X_target_plus = torch.concat([X, torch.zeros((1,), dtype=torch.float32, device='cuda')], -1)
+        q = torch.relu(self.linear_q(X_target_plus))  # q drop
+        k = X_target_plus
+        v = X_target_plus
+        # 这里舍弃掉了序列式的注意
+        attention_score = torch.matmul(q, k.transpose(1, 2)) / self.scale
+        # alpha
+        alpha = torch.sigmoid(self.linear_alpha(X_target_plus[:, -1, :])) + 1  # [1, 2]
+        attention_score = entmax_bisect(attention_score, alpha=alpha, dim=-1)
+        attention_result = torch.matmul(attention_score, v)
+        C_hat = self.layer_norm(self.ffn(attention_result) + attention_result)
+        C, target_emb = torch.chunk(C_hat, chunks=2, dim=1)
+        return C, target_emb
+        # target_emb = C_hat[:, -1, :]
+        # C = C_hat[:, :-1, :]
+
+
+class TargetEnhance(nn.Module):
+    def __init__(self, emb_size, gamma, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.emb_size = emb_size
+        self.gamma = gamma
+        self.linear_c = nn.Linear(self.emb_size * 4, self.emb_size * 2, bias=False)
+
+    def forward(self, C_last, C_target):
+        Sim = F.cosine_similarity(C_last, C_target)
+        threshold = self.gamma * torch.mean(torch.sum(Sim,0))
+        condition = (Sim - threshold) > 0
+         torch.where(condition, self.linear_c(torch.concat([C_last, C_target], dim=-1)), C_target)
 
 
 class TASI_GNN(nn.Module):
